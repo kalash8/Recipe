57375f3799b475d24b826060340031d56cc3b8f2
kalash8 <kalashtelkar@gmail.com>
2023-07-13 14:55:27 +0530

    connected mongodb compass

---
+++ server/src/Routes/users.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { UserModel } from '../models/users';
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username });
+
+    res.json(user);
+});
+
+
+router.post("/login");
+
+
+export { router as userRouter };
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from './Routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/auth", userRouter);
+
+mongoose.connect("mongodb+srv://kalashtelkar:RMBZnwMk4VY2GcEo@cluster0.cj0eso1.mongodb.net/Cluster0?retryWrites=true&w=majority");
+
 
 app.listen(3001, ()=> console.log("Server started!"));
 
